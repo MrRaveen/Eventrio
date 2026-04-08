29673750d0c7aa2c2caacf19a9ed0c9cddc013c8
MrRaveen <raveenjayasanka41@gmail.com>
2026-04-09 00:17:56 +0530

    added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    # app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    )
     app.config['SECRET_KEY'] = 'dev-secret-key'
 
     from app.db import init_db
